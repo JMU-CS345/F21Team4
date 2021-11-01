a18b52140aa9e2eaddbcebba56fbc94aa6708e07
ztuck9 <91218102+ztuck9@users.noreply.github.com>
2021-11-01 14:33:16 -0400

    Update DogDisplay.java

--- src/DogDisplay.java
+++ src/DogDisplay.java
@@ -1,8 +1,35 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 /**
  * A class.
+ * 
  * @author zacktucker
  *
  */
+
 public class DogDisplay {
 
+	private JFrame frame;
+	private final int windowWidth = 1024;
+	private final int windowHeight = 768;
+
+
+
+	public DogDisplay() {
+		
+	}
+
+	public void createAndShowGUI() {
+
+		// Create and set up the window.
+		frame = new JFrame("DogDisplay");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Display the window.
+		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
