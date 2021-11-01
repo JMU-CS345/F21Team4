4712c9f2504cf933b6af41adc645f75ddfd03043
agpolivka <91218149+agpolivka@users.noreply.github.com>
2021-11-01 14:48:31 -0400

    Updated method
    just added a method that will need to be completed

--- src/DogDisplay.java
+++ src/DogDisplay.java
@@ -1,6 +1,7 @@
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
+
+
 
 /**
  * A class.
@@ -11,25 +12,28 @@ import javax.swing.JFrame;
 
 public class DogDisplay {
 
-	private JFrame frame;
-	private final int windowWidth = 1024;
-	private final int windowHeight = 768;
+  private JFrame frame;
+  private final int windowWidth = 1024;
+  private final int windowHeight = 768;
+  private String dogNames[];
 
+  public DogDisplay() {
 
+  }
 
-	public DogDisplay() {
-		
-	}
+  public void createAndShowGUI() {
 
-	public void createAndShowGUI() {
+    // Create and set up the window.
+    frame = new JFrame("DogDisplay");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Create and set up the window.
-		frame = new JFrame("DogDisplay");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Display the window.
+    frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
+    frame.pack();
+    frame.setVisible(true);
+  }
 
-		// Display the window.
-		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
-		frame.pack();
-		frame.setVisible(true);
-	}
+  public void getDogNames() {
+
+  }
 }
