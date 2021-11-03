e156dfcf9493da366e9118ab8f28bb9ce7be6630
ztuck9 <91218102+ztuck9@users.noreply.github.com>
2021-11-03 15:01:20 -0400

    Put the list into the frame
    - Make it half the pane
    - Make it so you can click on it and have it display

--- src/DogDisplay.java
+++ src/DogDisplay.java
@@ -1,8 +1,7 @@
+import java.awt.Component;
 import java.awt.Dimension;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
-=======
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,13 +10,12 @@ import javax.swing.JFrame;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
->>>>>>> 763bc4e0220597d59ac9d5cc5985ee8f378447cb
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 
@@ -30,30 +28,26 @@ import javax.swing.event.ListSelectionEvent;
 
 public class DogDisplay {
 
-<<<<<<< HEAD
 	private JFrame frame;
 	private final int windowWidth = 1024;
 	private final int windowHeight = 768;
-	private JScrollPane scrollPane;
+	List<String> dogBreeds;
+	JList dogJList;
+	JScrollPane scrollPane;
 
-	public DogDisplay() {
+	public DogDisplay() throws IOException {
+		dogBreeds = new ArrayList<String>();
+		getDogNames();
+		dogJList = new JList(dogBreeds.toArray());
+        scrollPane = new JScrollPane(dogJList);
+
 	}
-=======
-  private JFrame frame;
-  private final int windowWidth = 1024;
-  private final int windowHeight = 768;
-  List<String> dogBreeds = new ArrayList<String>();
-  // private String[] dogNames;
-
-  public DogDisplay() {
-
-  }
->>>>>>> 763bc4e0220597d59ac9d5cc5985ee8f378447cb
 
 	public void createAndShowGUI() {
 
 		// Create and set up the window.
 		frame = new JFrame("DogDisplay");
+	    frame.add(this.scrollPane);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Display the window.
@@ -62,28 +56,20 @@ public class DogDisplay {
 		frame.setVisible(true);
 	}
 
-<<<<<<< HEAD
-	public void getDogNames() {
-=======
-  public void getDogNames() throws IOException {
-    StringBuilder sb = new StringBuilder();
-    String line;
-    URL url = new URL("https://api.thedogapi.com/v1/breeds");
+	public void getDogNames() throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String line;
+		URL url = new URL("https://api.thedogapi.com/v1/breeds");
 
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode tree = mapper.readTree(url);
 
+		for (int x = 0; x < tree.size(); x++) {
+			JsonNode breedNode = tree.get(x);
 
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode tree = mapper.readTree(url);
-    List<String> dogBreeds = new ArrayList<String>();
-
-    for (int x = 0; x < tree.size(); x++) {
-      JsonNode breedNode = tree.get(x);
-
-      String dogBreed = breedNode.get("name").asText();
-      this.dogBreeds.add(x, dogBreed);
-    }
-
->>>>>>> 763bc4e0220597d59ac9d5cc5985ee8f378447cb
+			String dogBreed = breedNode.get("name").asText();
+			this.dogBreeds.add(x, dogBreed);
+		}
 
 	}
 }
