ca63dcf4f996eb7ee87ce7cbf2ab5560807de764
ztuck9 <91218102+ztuck9@users.noreply.github.com>
2021-11-03 14:47:17 -0400

    random changes

--- src/DogDisplay.java
+++ src/DogDisplay.java
@@ -1,7 +1,14 @@
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-
-
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
 
 /**
  * A class.
@@ -12,28 +19,27 @@ import javax.swing.JFrame;
 
 public class DogDisplay {
 
-  private JFrame frame;
-  private final int windowWidth = 1024;
-  private final int windowHeight = 768;
-  private String[] dogNames;
+	private JFrame frame;
+	private final int windowWidth = 1024;
+	private final int windowHeight = 768;
+	private JScrollPane scrollPane;
 
-  public DogDisplay() {
+	public DogDisplay() {
+	}
 
-  }  
+	public void createAndShowGUI() {
 
-  public void createAndShowGUI() {
+		// Create and set up the window.
+		frame = new JFrame("DogDisplay");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    // Create and set up the window.
-    frame = new JFrame("DogDisplay");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Display the window.
+		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-    // Display the window.
-    frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
-    frame.pack();
-    frame.setVisible(true);
-  }
+	public void getDogNames() {
 
-  public void getDogNames() {
-
-  }
+	}
 }
