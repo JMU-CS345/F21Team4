c8ce349348073ca3db840dff5a33fadc7f177d54
agpolivka <91218149+agpolivka@users.noreply.github.com>
2021-11-05 14:19:47 -0400

    links for doc pics exist

--- src/DogDisplay.java
+++ src/DogDisplay.java
@@ -1,23 +1,13 @@
-import java.awt.Component;
 import java.awt.Dimension;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JFrame;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * A class.
@@ -28,48 +18,70 @@ import javax.swing.event.ListSelectionEvent;
 
 public class DogDisplay {
 
-	private JFrame frame;
-	private final int windowWidth = 1024;
-	private final int windowHeight = 768;
-	List<String> dogBreeds;
-	JList dogJList;
-	JScrollPane scrollPane;
+  private JFrame frame;
+  private final int windowWidth = 1024;
+  private final int windowHeight = 768;
+  List<String> dogBreeds;
+  List<URL> dogPictures;
+  JList dogJList;
+  JScrollPane scrollPane;
 
-	public DogDisplay() throws IOException {
-		dogBreeds = new ArrayList<String>();
-		getDogNames();
-		dogJList = new JList(dogBreeds.toArray());
-        scrollPane = new JScrollPane(dogJList);
+  public DogDisplay() throws IOException {
+    dogBreeds = new ArrayList<String>();
+    getDogNames();
+    dogJList = new JList(dogBreeds.toArray());
+    scrollPane = new JScrollPane(dogJList);
 
-	}
+  }
 
-	public void createAndShowGUI() {
+  public void createAndShowGUI() {
 
-		// Create and set up the window.
-		frame = new JFrame("DogDisplay");
-	    frame.add(this.scrollPane);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Create and set up the window.
+    frame = new JFrame("DogDisplay");
+    frame.add(this.scrollPane);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Display the window.
-		frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
-		frame.pack();
-		frame.setVisible(true);
-	}
+    // Display the window.
+    frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
+    frame.pack();
+    frame.setVisible(true);
+  }
 
-	public void getDogNames() throws IOException {
-		StringBuilder sb = new StringBuilder();
-		String line;
-		URL url = new URL("https://api.thedogapi.com/v1/breeds");
+  public void getDogNames() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    String line;
+    URL url = new URL("https://api.thedogapi.com/v1/breeds");
 
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode tree = mapper.readTree(url);
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode tree = mapper.readTree(url);
 
-		for (int x = 0; x < tree.size(); x++) {
-			JsonNode breedNode = tree.get(x);
+    for (int x = 0; x < tree.size(); x++) {
+      JsonNode breedNode = tree.get(x);
 
-			String dogBreed = breedNode.get("name").asText();
-			this.dogBreeds.add(x, dogBreed);
-		}
+      String dogBreed = breedNode.get("name").asText();
+      this.dogBreeds.add(x, dogBreed);
+    }
 
-	}
+  }
+
+  public void getDogPhotoURL() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    String line;
+    URL url = new URL("https://api.thedogapi.com/v1/breeds");
+
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode tree = mapper.readTree(url);
+
+
+
+    for (int x = 0; x < tree.size(); x++) {
+      JsonNode jsonNode = tree.get("photo_url");
+
+      URL urlDogPics = null;
+      String urlString = jsonNode.asText();
+      urlDogPics = new URL(urlString);
+      this.dogPictures.add(x, urlDogPics);
+    }
+
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     DogDisplay dd = new DogDisplay();
     dd.createAndShowGUI();
 
-    dd.getDogNames();
+
 
   }
 
