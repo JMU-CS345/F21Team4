84c9096af6b0e00b7924aef23103c1a5fce8a27a
ztuck9 <91218102+ztuck9@users.noreply.github.com>
2021-11-08 14:14:42 -0500

    Create Dog.java

---
+++ src/Dog.java
@@ -0,0 +1,57 @@
+import java.net.URL;
+
+/**
+ * 
+ * @author zacktucker
+ *
+ */
+public class Dog {
+	private final String name;
+	private final URL url;
+	private final String height;
+	private final String weight;
+	private final String origin;
+	private final String lifespan;
+	private final String description;
+
+	public Dog(String name, URL url, String height, String weight, String origin, String lifespan, String description) {
+
+		this.name = name;
+		this.url = url;
+		this.height = height;
+		this.weight = weight;
+		this.origin = origin;
+		this.lifespan = lifespan;
+		this.description = description;
+
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public URL getURL() {
+		return this.url;
+	}
+
+	public String getHeight() {
+		return this.height;
+	}
+
+	public String getWeight() {
+		return this.weight;
+	}
+
+	public String getOrigin() {
+		return this.origin;
+	}
+
+	public String getLifespan() {
+		return this.lifespan;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+}
