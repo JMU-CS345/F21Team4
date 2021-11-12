5f4ed58f4692f9d951803872df164cb526ca9703
Mattwong37 <mattwong37@gmail.com>
2021-11-11 19:33:42 -0500

    Update Window.java

--- ui/Window.java
+++ ui/Window.java
@@ -1,5 +1,39 @@
+import java.awt.Dimension;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+/**
+ * Window class
+ * 
+ * @author Zack Tucker, Matt Wong, Thomas Mandell, Alex Polivka, Jonathan Wist
+ * @version Nov 1, 2021
+ */
 
 public class Window
 {
+  private JFrame frame;
+  private final int windowWidth = 1024;
+  private final int windowHeight = 768;
+  private DogDisplayAsAPane test;
+
+  public Window() throws IOException
+  {
+    test = new DogDisplayAsAPane();
+    // Create and set up the window.
+    frame = new JFrame("DogDisplay");
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // Display the window.
+    frame.setMinimumSize(new Dimension(windowWidth, windowHeight));
+    frame.pack();
+    frame.setVisible(true);
+
+    frame.setLayout(new BorderLayout());
+
+    frame.getContentPane().add(test);
+  }
 
 }
