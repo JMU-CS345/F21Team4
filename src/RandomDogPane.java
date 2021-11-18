35f1fac1bb98ea3cbbfdb1b9a41acb5213be0b04
agpolivka <91218149+agpolivka@users.noreply.github.com>
2021-11-17 20:54:44 -0500

    New class
    random dog picture pane

---
+++ src/RandomDogPane.java
@@ -0,0 +1,36 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class RandomDogPane extends JPanel implements ListSelectionListener {
+
+  String urlString;
+  URL urlDogPic;
+  JButton random = new JButton("Random Dog Picture");
+  JPanel dogRandom = new JPanel();
+
+
+  private final int windowWidth = 1024;
+  private final int windowHeight = 768;
+
+  public RandomDogPane() {
+
+  }
+
+
+  @Override
+  public void valueChanged(ListSelectionEvent e) {
+    // TODO Auto-generated method stub
+    urlString = "https://dog.ceo/api/breeds/image/random";
+    try {
+      urlDogPic = new URL(urlString);
+    } catch (MalformedURLException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+  }
+
+}
