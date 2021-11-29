2f9bbc723cdef25ed43903d3e6eb6d67d76efe75
ztuck9 <91218102+ztuck9@users.noreply.github.com>
2021-11-29 14:36:43 -0500

    Utility files for making memes.

---
+++ bin/Image.java
@@ -0,0 +1,200 @@
+/*
+ * This work complies with the JMU Honor Code.
+ * 
+ * References and Acknowledgments: I received no outside help with this
+ * programming assignment.
+ */
+
+/**
+ * A class that represents an image object made up of pixel objects.
+ * 
+ * @author Zack Tucker
+ * @version 2-3-21
+ */
+
+public class Image {
+    private Pixel[][] pixels;
+
+    /**
+     * Default constructor for image, sets all RGB to 255.
+     * 
+     * @param width width of the image
+     * @param height height of the image
+     */
+
+    public Image(int width, int height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException(
+                    "width & height must be " + "greater than 0");
+        }
+
+        this.pixels = new Pixel[width][height];
+
+        for (int i = 0; i < this.getWidth(); i++) {
+            for (int x = 0; x < this.getHeight(); x++) {
+                this.pixels[i][x] = new Pixel(255, 255, 255);
+            }
+        }
+
+    }
+
+    /**
+     * Explicit constructor for Image.
+     * 
+     * @param width the width of the image
+     * @param height the height of the image
+     * @param red the red value of the image
+     * @param green the green value of the image
+     * @param blue the blue value of the image
+     */
+
+    public Image(int width, int height, int red, int green, int blue) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException(
+                    "width & height must be " + "greater than 0");
+        }
+
+        this.pixels = new Pixel[width][height];
+
+        for (int i = 0; i < this.getWidth(); i++) {
+            for (int x = 0; x < this.getHeight(); x++) {
+                this.pixels[i][x] = new Pixel(red, green, blue);
+            }
+        }
+
+    }
+
+    /**
+     * Gets the pixel at the specified x and y coordinates.
+     * 
+     * @param x the x coordinate of the pixel
+     * @param y the y coordinate of the pixel
+     * @return the pixel at the specified x and y coordinates.
+     */
+
+    public Pixel getPixel(int x, int y) {
+        if (x < this.getWidth() && x > -1 && y < this.getHeight() && y > -1) {
+            return this.pixels[x][y];
+        } else {
+            return null;
+        }
+
+    }
+
+    /**
+     * Sets the pixel at the specified x and y coordinates.
+     * 
+     * @param x the x coordinate of the pixel
+     * @param y the y coordinate of the pixel
+     * @param pixel the pixel to insert at the specified coordinates.
+     */
+
+    public void setPixel(int x, int y, Pixel pixel) {
+        if (pixel != null && x < this.getWidth() && x > -1
+                && y < this.getHeight() && y > -1) {
+            this.pixels[x][y] = pixel;
+        }
+    }
+
+    /**
+     * Getter for height of image.
+     * 
+     * @return height of image
+     */
+
+    public int getHeight() {
+        return this.pixels[0].length;
+    }
+
+    /**
+     * Getter for width of image.
+     * 
+     * @return width of image.
+     */
+
+    public int getWidth() {
+        return this.pixels.length;
+    }
+
+    /**
+     * equals method to test if two images are object.
+     * 
+     * @param other as the image to check against
+     * @return true if the images are the same, false if not.
+     */
+
+    public boolean equals(Object other) {
+        boolean token = false;
+        if (!(other instanceof Image)) {
+            return false;
+        }
+
+        Image otherImage = (Image) other;
+        int count = 0;
+        if (otherImage.getHeight() == this.getHeight()
+                && otherImage.getWidth() == this.getWidth()) {
+
+            for (int i = 0; i < this.getWidth(); i++) {
+                for (int x = 0; x < this.getHeight(); x++) {
+                    Pixel otherP = otherImage.getPixel(i, x);
+
+                    if (otherP.equals(this.getPixel(i, x))) {
+                        count++;
+                    }
+
+                }
+
+            }
+
+        }
+
+        if (this.getWidth() * this.getHeight() == count) {
+            token = true;
+        } else {
+            token = false;
+        }
+
+        return token;
+
+    }
+
+    /**
+     * Returns a toString representation of the image.
+     * 
+     * @return a string representing the image
+     */
+
+    public String toString() {
+        return "<Image width=" + this.getWidth() + " height=" + this.getHeight()
+                + ">";
+    }
+
+    /**
+     * Returns a toString of the image as a 2d.
+     * 
+     * @return the image as a 2d array in string form.
+     */
+
+    public String toStringDebug() {
+        StringBuilder str = new StringBuilder();
+        str.append("<Image width=" + this.getWidth() + " height="
+                + this.getHeight() + ">\n");
+
+        for (int i = 0; i < this.getWidth(); i++) {
+            for (int x = 0; x < this.getHeight(); x++) {
+                str.append(" ");
+                str.append(this.pixels[i][x].toString());
+                if (x + 1 == this.getHeight()) {
+                    str.append("\n");
+
+                }
+            }
+
+        }
+
+        String r = str.toString();
+        return r;
+
+    }
+
+}
---
+++ bin/ImageUtils.java
@@ -0,0 +1,59 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+/**
+ * Utility class for converting back and forth between Image objects and
+ * java.awt.image.BufferedImage objects.
+ * 
+ * @author CS 159 Instructors
+ * @version V1.0, 3/7/2021
+ * 
+ */
+public class ImageUtils {
+
+    /**
+     * Convert from java.awt.image.BufferedImage to Image.
+     * 
+     * @param bufferedImage The BufferedImage to convert.
+     * @return Equivalent Image.
+     */
+    public static Image bufferedImageToImage(BufferedImage bufferedImage) {
+        Image newImage = new Image(bufferedImage.getWidth(),
+                bufferedImage.getHeight());
+        Color color;
+
+        for (int y = 0; y < bufferedImage.getHeight(); y++) {
+            for (int x = 0; x < bufferedImage.getWidth(); x++) {
+                color = new Color(bufferedImage.getRGB(x, y));
+                newImage.setPixel(x, y, new Pixel(color.getRed(),
+                        color.getGreen(), color.getBlue()));
+            }
+        }
+        return newImage;
+
+    }
+
+    /**
+     * Convert from Image to java.awt.image.BufferedImage.
+     * 
+     * @param image The Image to convert.
+     * @return Equivalent BufferedImage
+     */
+    public static BufferedImage imageToBufferedImage(Image image) {
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(),
+                image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Pixel pixel;
+        Color color;
+
+        for (int y = 0; y < bufferedImage.getHeight(); y++) {
+            for (int x = 0; x < bufferedImage.getWidth(); x++) {
+                pixel = image.getPixel(x, y);
+                color = new Color(pixel.getRed(), pixel.getGreen(),
+                        pixel.getBlue());
+                bufferedImage.setRGB(x, y, color.getRGB());
+            }
+        }
+        return bufferedImage;
+
+    }
+}
